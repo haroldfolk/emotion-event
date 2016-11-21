7c06a288ff05efbd95badba8a471d284b6d2f810
haroldfolk <haroldfolk@gmail.com>
2016-11-21 09:30:55 -0400

    commit 1

---
+++ views/site/index.php
@@ -0,0 +1,45 @@
+<?php
+use yii\httpclient\Client;
+
+/* @var $this yii\web\View */
+
+$this->title = 'My Yii Application';
+?>
+<div class="site-index">
+
+    <div class="body-content">
+        <?php
+        $im = file_get_contents('filename.jpg');
+        $imdata = base64_encode($im);
+
+        //        echo $imdata;
+        //        echo "<img src='data:image/jpg;base64,".$imdata."' />";
+        $data = '{
+            "requests":[
+    {
+        "image":{
+        "content":"' . $imdata . '"
+      },
+      "features":[
+        {
+            "type":"TEXT_DETECTION",
+          "maxResults":1
+        }
+      ]
+    }
+  ]
+}';
+        $client = new Client();
+        $response = $client->createRequest()
+            ->setMethod('post')
+            ->addHeaders(['content-type' => 'application/json'])
+            ->setUrl('https://vision.googleapis.com/v1/images:annotate?key=AIzaSyDSTRujOfmCPu0B3iZF-0wFFpLhVfDYBOk')
+            ->setContent($data)
+            ->send();
+        if ($response->isOk) {
+//           $decode=json_decode($response->content,true);
+           echo $response->getData()->toString();
+        }
+        ?>
+    </div>
+</div>
---
+++ views/site/uploader.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Harold
+ * Date: 21/11/2016
+ * Time: 3:48
+ */
+$target_path = "uploads/";
+$target_path = $target_path . basename( $_FILES['uploadedfile']['name']);
+if(move_uploaded_file($_FILES['uploadedfile']['tmp_name'], $target_path)) { 
+    echo "El archivo ". basename( $_FILES['uploadedfile']['name']). " ha sido subido";
+} else{
+    echo "Ha ocurrido un error, trate de nuevo!";
+}
