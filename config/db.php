0e7b975b84de714e14debd26bba45850751f2b36
HDSC <haroldfolk@gmail.com>
2016-11-27 17:20:30 -0400

    321

--- config/db.php
+++ config/db.php
@@ -4,6 +4,6 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=segparcial',
     'username' => 'root',
-    'password' => '',
+    'password' => 'yohanita',
     'charset' => 'utf8',
 ];
