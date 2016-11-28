6ed5e4dc9a22c8dec37a6b85e5457d2aaa76af36
haroldfolk@gmail.com <haroldfolk@gmail.com>
2016-11-28 09:35:37 -0400

    inplementando interpretador 2.4 mau

--- controllers/ApiController.php
+++ controllers/ApiController.php
@@ -72,8 +72,8 @@ class ApiController extends Controller
         $result = $vision->annotate($image);
 
         unlink($file);
-        return $this->interpretar($result->info()['textAnnotations']);
-
+//        return $this->interpretar($result->info()['textAnnotations']);
+        return $this->getData($result->info()['textAnnotations']);
 //        return $result->info();
 
 
@@ -171,5 +171,151 @@ class ApiController extends Controller
 
 
         return $return;
+    }
+    public function getData($imgPath)
+    {
+//        $textArray = $this->OCR($imgPath);
+        $textArray=$imgPath;
+        $state = 0;
+        $AC = "";
+        $data = [];
+        foreach ($textArray as $text) {
+            $word = $text['description'];
+            switch ($state) {
+                case 0: // Caso base
+                    if ($word === 'firma' || $word === 'firma,')
+                        $state = 1; // Para Confirmar
+                    else if ($word === 'fotografia')
+                        $state = 2;
+                    else if ($word === 'impresion')
+                        $state = 3;
+                    else if ($word === 'Civil')
+                        $state = 10;
+                    else if (strpos($word, "Profesion") !== false && count($data) > 0)
+                        $state = 11;
+                    break;
+                case 1: // Confirmar carnet
+                    if ($word === 'fotografia')
+                        $state = 2;
+                    break;
+                case 2: // Carnet
+                    if (is_numeric($word))
+                        $data['CI'] = intval($word);
+                    $AC = "";
+                    $state = 0;
+                    break;
+                case 3: // Despues de impresion
+                    if (!is_numeric($word) && $word !== "A" && $word !== "A:" && $word !== "pertenece") {
+                        $AC = $word . " ";
+                        $state = 4;
+                    }
+                    break;
+                case 4: // Concatenar el Nombre
+                    if ($word === 'Nacido') {
+                        $state = 5;
+                        $data['NAME'] = substr($AC, 0, -1);
+                        $AC = "";
+                    } else {
+                        $AC = $AC . $word . " ";
+                    }
+                    break;
+                case 5: // Revisar dia
+                    if (is_numeric($word)) { //Dia
+                        $AC = $word . ',';
+                        $state = 6;
+                    }
+                    break;
+                case 6: // Revisar mes
+                    if ($word !== 'de') {
+                        $AC = $AC . $this->getMonth($word) . ',';
+                        $state = 7;
+                    }
+                    break;
+                case 7: // Revisar year
+                    if ($word !== 'de') {
+                        $AC = $AC . $word;
+                        $dateArray = explode(",", $AC);
+                        $data['BIRTHDAY'] = $dateArray[2] . "-" . $dateArray[1] . "-" . $dateArray[0];
+                        $state = 8;
+                    }
+                    break;
+                case 8: // Revisar Lugar de nacimiento
+                    if ($word !== "En") {
+                        $AC = $word . " ";
+                        $state = 9;
+                    }
+                    break;
+                case 9: // Concatenar Lugar de Nacimiento
+                    if ($word !== "Estado")
+                        $AC = $AC . $word . " ";
+                    else {
+                        $data['BIRTHPLACE'] = substr($AC, 0, -1);
+                        $state = 0;
+                        $AC = "";
+                    }
+                    break;
+                case 10: // Estado Civil
+                    $data['CIVIL_STATUS'] = $word;
+                    $AC = "";
+                    $state = 0;
+                    break;
+                case 11:
+                    if ($word !== "Domicilio")
+                        $AC = $AC . $word . " ";
+                    else {
+                        $data['OCCUPATION'] = substr($AC, 0, -1);
+                        $state = 0;
+                        $AC = "";
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+        return $data;
+    }
+
+    private function getMonth($word)
+    {
+        $month = "";
+        switch ($word) {
+            case 'Enero':
+                $month = "01";
+                break;
+            case 'Febrero':
+                $month = "02";
+                break;
+            case 'Marzo':
+                $month = "03";
+                break;
+            case 'Abril':
+                $month = "04";
+                break;
+            case 'Mayo':
+                $month = "05";
+                break;
+            case 'Junio':
+                $month = "06";
+                break;
+            case 'Julio':
+                $month = "07";
+                break;
+            case 'Agosto':
+                $month = "08";
+                break;
+            case 'Septiembre':
+                $month = "09";
+                break;
+            case 'Octubre':
+                $month = "10";
+                break;
+            case 'Noviembre':
+                $month = "11";
+                break;
+            case 'Diciembre':
+                $month = "12";
+                break;
+        }
+        return $month;
     }
 }
