da286ddceb359051c5395043bb2c3c1b9b32ce97
harold <haroldfolk@gmail.com>
2016-12-10 03:39:44 -0400

    PF->probando maps

--- views/site/miindex.php
+++ views/site/miindex.php
@@ -1,42 +1,10 @@
-<!DOCTYPE html>
+<?php
+use voime\GoogleMaps\Map;
 
-<html lang="es">
-<head>
-
-
-    <meta charset="utf-8">
-
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Google Maps Localizador</title>
-
-    <script src="https://maps.googleapis.com/maps/api/js"></script>
-    <!--    <link rel="stylesheet" type="text/css" href="estilos.css">-->
-
-</head>
-<body>
-
-<strong>Museo del Prado </strong>/ Latitud: 40.413740 / Longitud: -3.6921 / Zoom: 18
-<div id="mapa"></div>
-
-
-<script>
-
-    function initialize() {
-        var mapa = document.getElementById('mapa');
-
-        var mapOptions = {
-            center: new google.maps.LatLng(40.413740, -3.6921),
-
-            zoom: 18,
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-
-        }
-        var mapa = new google.maps.Map(mapa, mapOptions)
-
-    }
-    google.maps.event.addDomListener(window, 'load', initialize);
-
-</script>
-
-</body>
-</html>
+echo Map::widget([
+    'zoom' => 16,
+    'center' => 'Red Square',
+    'width' => '700px',
+    'height' => '400px',
+    'mapType' => Map::MAP_TYPE_SATELLITE,
+]);
