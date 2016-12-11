a561dc79ebddc0ca08a18f1fd5db2030195de4f2
harold <haroldfolk@gmail.com>
2016-12-11 12:07:59 -0400

    con upload de inmagenes             y url evento y videos probando

--- views/evento/upload.php
+++ views/evento/upload.php
@@ -13,7 +13,7 @@ use yii\widgets\ActiveForm;
 <?php $form = ActiveForm::begin(['options' => ['enctype' => 'multipart/form-data']]) ?>
 
 <? //= $form->field($model, 'imageFiles[]')->fileInput(['multiple' => true, 'accept' => 'image/*']) ?>
-<?= $form->field($model, 'imageFiles[]')->fileInput(['multiple' => true]) ?>
+<?= $form->field($model, 'imageFiles[]')->fileInput(['multiple' => true, 'accept' => 'image/*']) ?>
 
     <button>Subir</button>
 
