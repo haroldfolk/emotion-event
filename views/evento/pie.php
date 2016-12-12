ed4aa3cfb9b947e4acc4565f43e6d263d39b9071
harold <haroldfolk@gmail.com>
2016-12-11 23:34:57 -0400

    creando tabla promedio y probando trigerphp+idPromedio

--- views/evento/pie.php
+++ views/evento/pie.php
@@ -1,9 +1,50 @@
 <?php
 use sjaakp\gcharts\PieChart;
-
+use sjaakp\gcharts\BarChart;
+use sjaakp\gcharts\ScatterChart;
+use sjaakp\gcharts\LineChart;
 ?>
     ...
 <?= PieChart::widget([
+    'height' => '400px',
+    'dataProvider' => $dataProvider,
+    'columns' => [
+        'nombre:string',
+        'valor'
+    ],
+    'options' => [
+        'title' => 'Eventos by Emociones'
+
+    ],
+]) ?>
+
+<?= BarChart::widget([
+    'height' => '400px',
+    'dataProvider' => $dataProvider,
+    'columns' => [
+        'nombre:string',
+        'valor'
+    ],
+    'options' => [
+        'title' => 'Eventos by Emociones'
+
+    ],
+]) ?>
+
+<?= ScatterChart::widget([
+    'height' => '400px',
+    'dataProvider' => $dataProvider,
+    'columns' => [
+        'nombre:string',
+        'valor'
+    ],
+    'options' => [
+        'title' => 'Eventos by Emociones'
+
+    ],
+]) ?>
+
+<?= LineChart::widget([
     'height' => '400px',
     'dataProvider' => $dataProvider,
     'columns' => [
