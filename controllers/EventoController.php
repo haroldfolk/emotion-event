4988cf8273fba444ae486f3f920712877daa7069
harold <haroldfolk@gmail.com>
2016-12-12 12:45:00 -0400

    ahora si listo pdf

--- controllers/EventoController.php
+++ controllers/EventoController.php
@@ -380,14 +380,14 @@ class EventoController extends Controller
         $pdf->SetFont('Arial', 'B', 16);
         $pdf->Cell(0, 0);
 
-        $pdf->Image("https://s3-us-west-2.amazonaws.com/fotowebhd/caratula.jpg", null, null, 300, 200, '', '');
+        $pdf->Image("https://s3-us-west-2.amazonaws.com/fotowebhd/caratula.jpg", 0, 0, 300, 200, '', '');
 
 
         $pdf->AddPage();
         $pdf->SetFont('Arial', 'B', 16);
         $pdf->Cell(0, 0, "Grafico 1");
 
-        $pdf->Image("https://s3-us-west-2.amazonaws.com/fotowebhd/pdf1.jpg", null, null, 300, 200, '', '');
+        $pdf->Image("https://s3-us-west-2.amazonaws.com/fotowebhd/pdf1.jpg", 0, 0, 300, 200, '', '');
 
 /////////////////////////////////
 
@@ -396,7 +396,7 @@ class EventoController extends Controller
         $pdf->SetFont('Arial', 'B', 16);
         $pdf->Cell(0, 0, "Grafico 2");
 
-        $pdf->Image("https://s3-us-west-2.amazonaws.com/fotowebhd/pdf2.jpg", null, null, 300, 200, '', '');
+        $pdf->Image("https://s3-us-west-2.amazonaws.com/fotowebhd/pdf2.jpg", 0, 0, 300, 200, '', '');
 
 /////////////////////////////////
 
@@ -404,7 +404,7 @@ class EventoController extends Controller
         $pdf->SetFont('Arial', 'B', 16);
         $pdf->Cell(0, 0, "Grafica 3");
 //
-        $pdf->Image("https://s3-us-west-2.amazonaws.com/fotowebhd/pdf3.jpg", null, null, 300, 200, '', '');
+        $pdf->Image("https://s3-us-west-2.amazonaws.com/fotowebhd/pdf3.jpg", 0, 0, 300, 200, '', '');
 /////////////////////////////////
 
 
@@ -412,7 +412,7 @@ class EventoController extends Controller
         $pdf->SetFont('Arial', 'B', 16);
         $pdf->Cell(0, 0, "Grafico 4");
 
-        $pdf->Image("https://s3-us-west-2.amazonaws.com/fotowebhd/pdf4.jpg", null, null, 300, 200, '', '');
+        $pdf->Image("https://s3-us-west-2.amazonaws.com/fotowebhd/pdf4.jpg", 0, 0, 300, 200, '', '');
 
 
         $pdf->Output();
