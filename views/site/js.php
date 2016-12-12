37013118094252e49fe95bf6cc1f1f747fdd5fb2
harold <haroldfolk@gmail.com>
2016-12-12 09:34:12 -0400

    probando js iframe

--- views/site/js.php
+++ views/site/js.php
@@ -1,2 +1,2 @@
-<iframe width="100%" height="300" src="//jsfiddle.net/eB2RX/1/embedded/" allowfullscreen="allowfullscreen"
+<iframe width="100%" height="500" src="//jsfiddle.net/eB2RX/1/embedded/result/" allowfullscreen="allowfullscreen"
         frameborder="0"></iframe>
