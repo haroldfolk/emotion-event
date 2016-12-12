0526af1ebddae59bca35945693337b377b2020bc
harold <haroldfolk@gmail.com>
2016-12-12 00:06:22 -0400

    listas EStADISTICAS OKOKOKOK empezando con pdf no da

--- views/evento/piepdf.php
+++ views/evento/piepdf.php
@@ -18,42 +18,42 @@ use sjaakp\gcharts\LineChart;
 
     ],
 ]) ?>
-
-<?= BarChart::widget([
-    'height' => '400px',
-    'dataProvider' => $dataProvider,
-    'columns' => [
-        'nombre:string',
-        'valor'
-    ],
-    'options' => [
-        'title' => 'Eventos by Emociones'
-
-    ],
-]) ?>
-
-<?= ScatterChart::widget([
-    'height' => '400px',
-    'dataProvider' => $dataProvider,
-    'columns' => [
-        'nombre:string',
-        'valor'
-    ],
-    'options' => [
-        'title' => 'Eventos by Emociones'
-
-    ],
-]) ?>
-
-<?= LineChart::widget([
-    'height' => '400px',
-    'dataProvider' => $dataProvider,
-    'columns' => [
-        'nombre:string',
-        'valor'
-    ],
-    'options' => [
-        'title' => 'Eventos by Emociones'
-
-    ],
-]) ?>
+<!---->
+<? //= BarChart::widget([
+//    'height' => '400px',
+//    'dataProvider' => $dataProvider,
+//    'columns' => [
+//        'nombre:string',
+//        'valor'
+//    ],
+//    'options' => [
+//        'title' => 'Eventos by Emociones'
+//
+//    ],
+//]) ?>
+<!---->
+<? //= ScatterChart::widget([
+//    'height' => '400px',
+//    'dataProvider' => $dataProvider,
+//    'columns' => [
+//        'nombre:string',
+//        'valor'
+//    ],
+//    'options' => [
+//        'title' => 'Eventos by Emociones'
+//
+//    ],
+//]) ?>
+<!---->
+<? //= LineChart::widget([
+//    'height' => '400px',
+//    'dataProvider' => $dataProvider,
+//    'columns' => [
+//        'nombre:string',
+//        'valor'
+//    ],
+//    'options' => [
+//        'title' => 'Eventos by Emociones'
+//
+//    ],
+//]) ?>
